4a4e22669e2bdce2a29475f91ef5db3a67fdf192
Егор KislBall <kislball@yahoo.com>
2020-03-27 18:35:45 +0300

    moved leveling to another file

---
+++ commands/i_balanceLeveling.js
@@ -0,0 +1,11 @@
+const Keyv = require("keyv");
+const client = require("../index.js");
+
+client.on('message', async msg => {
+	if(msg.channel == '671255366180405258') return
+	if(msg.channel == '669722735357722624') return
+	if(msg.content.startsWith(process.env.PREFIX)) return
+	const db = new Keyv(process.env.MONEY_DB);
+	let balance = (await db.get(msg.author.id) || 0);
+	db.set(msg.author.id, balance + 1);
+});
