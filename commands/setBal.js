c06d72accab13d00a98fb5016f4c3356bb00402a
AlexGitHub-1 <kan200624@gmail.com>
2020-03-28 21:29:41 +0300

    fix fixed:D

--- commands/setBal.js
+++ commands/setBal.js
@@ -4,7 +4,8 @@ module.exports = {
     execute: async message => {
         let users = ["427518363955363841","419524085736013834"];
         if(!users.includes(message.author.id)) return message.channel.send(":x: У вас недостаточно прав для использования этой команды!")
-        const member = message.mentions.members.first() || message.member;
+        let member = message.mentions.members.first();
+        if(!member) return message.channel.send(":x: Упомяните участника!");
         let args = message.content.split(" ");
         if(!parseInt(args[2])) return message.channel.send(":x: Укажите число!");
         const db = new Keyv(process.env.MONEY_DB);
