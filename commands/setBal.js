8b5b7f666bc4b849a1aec50d9e3c4e14c0d5a405
AlexGitHub-1 <kan200624@gmail.com>
2020-03-28 21:37:09 +0300

    hui poimi chto

--- commands/setBal.js
+++ commands/setBal.js
@@ -2,8 +2,10 @@ const Keyv = require("keyv");
 
 module.exports = {
     execute: async message => {
-        let users = ["427518363955363841","419524085736013834"];
-        if(!users.includes(message.author.id)) return message.channel.send(":x: У вас недостаточно прав для использования этой команды!")
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":x: У вас недостаточно прав для использования этой команды!");
+       //если будут использовать не по назначению , то заберем
+        //let users = ["427518363955363841","419524085736013834"];
+        //if(!users.includes(message.author.id)) return message.channel.send(":x: У вас недостаточно прав для использования этой команды!")
         let member = message.mentions.members.first();
         if(!member) return message.channel.send(":x: Упомяните участника!");
         let args = message.content.split(" ");
