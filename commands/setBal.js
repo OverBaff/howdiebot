7ac13840c14991e9eadf6cad36513dba42ce72e8
AlexGitHub-1 <kan200624@gmail.com>
2020-03-28 21:43:30 +0300

    анти-пробелы

--- commands/setBal.js
+++ commands/setBal.js
@@ -8,8 +8,8 @@ module.exports = {
         //if(!users.includes(message.author.id)) return message.channel.send(":x: У вас недостаточно прав для использования этой команды!")
         let member = message.mentions.members.first();
         if(!member) return message.channel.send(":x: Упомяните участника!");
-        let args = message.content.split(" ");
-        if(!parseInt(args[2])) return message.channel.send(":x: Укажите число!");
+        let args = message.content.split(" ").slice(2);
+        if(!parseInt(args)) return message.channel.send(":x: Укажите число!");
         const db = new Keyv(process.env.MONEY_DB);
         db.set(member.user.id, parseInt(args[2])).then(() => {
             message.channel.send(":white_check_mark: Успех!");
