3e3e65e993cf647ff174295db4268be822aea543
AlexGitHub-1 <kan200624@gmail.com>
2020-03-28 21:22:30 +0300

    owners

--- commands/setBal.js
+++ commands/setBal.js
@@ -2,7 +2,8 @@ const Keyv = require("keyv");
 
 module.exports = {
     execute: async message => {
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":x: Нет прав!");
+        let users = ["427518363955363841","419524085736013834"];
+        if(!users.includes(message.author.id)) return message.channel.send(":x: У вас недостаточно прав для использования этой команды!")
         let member = message.mentions.members.first();
         if(!member) return message.channel.send(":x: Упомяните участника!");
         let args = message.content.split(" ");
